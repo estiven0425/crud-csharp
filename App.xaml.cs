f344f66666637988a1d7a16302a00a190e1e10b3
estiven0425 <estivenmoto6@gmail.com>
2025-11-19 10:12:34 -0500

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace crud_csharp;
+
+public partial class App : Application
+{
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace crud_csharp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
